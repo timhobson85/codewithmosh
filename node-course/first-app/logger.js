665964f9559490538495bbebba9b60042cedea31
Tim Hobson <claehobson@gmail.com>
2020-05-16 02:08:46 +1000

    up to 03-04

--- node-course/first-app/logger.js
+++ node-course/first-app/logger.js
@@ -1,10 +1,18 @@
 console.log(__filename)
 console.log(__dirname)
+const EventEmitter = require('events');
+
 let url = 'http://mylogger.io/log';
 
-function log(message) {
-  // Send a HTTP request.
-  console.log(message);
+class Logger extends EventEmitter {
+  log(message) {
+    // Send a HTTP request.
+    console.log(message);
+   
+    // Raise: logging (data: message)
+    this.emit('messageLogged', {id: 1, url:'hello'})
+  };
+
 };
 
-module.exports = log;
+module.exports = Logger;
